36e1833ce93f5e2de531e9d4a79bc1837b8f7c2f
Anders Andersson <trezker@gmail.com>
2010-04-20 10:39:35 +0200

    Automatic fitting of object into interface viewport

--- save_load.lua
+++ save_load.lua
@@ -71,12 +71,46 @@ load_animated_object = function(path)
 	om.bbox:set_color(1, 0, 0, 0.5)
 	--End of bounding box code
 
+	size = high - low
+	rotx = 0
+	roty = 0
+	if size.x < size.z and size.x < size.y then
+		roty = 90
+	elseif size.y < size.z and size.y < size.x then
+		rotx = 90
+	end
+	biggest = size.x
+	if size.y > biggest then
+		biggest = size.y
+	elseif size.z > biggest then
+		biggest = size.z
+	end
+	scale = 1/biggest
+	print(biggest)
+	
+	center = (low+high)/2*scale
+	print("Center: " .. center.x .. ", " .. center.y .. ", " .. center.z)
+
 	om.interface_transform = alledge_lua.transformnode.new()
-	om.interface_transform:set_position(alledge_lua.vector3.new(0, -6, -20))
+	om.interface_transform:set_scale(alledge_lua.vector3.new(scale, scale, scale))
+	if roty == 90 then
+		om.interface_transform:set_position(alledge_lua.vector3.new(-center.z, -center.y, -2))
+	elseif rotx == 90 then
+		om.interface_transform:set_position(alledge_lua.vector3.new(-center.x, -center.z, -2))
+	else
+		om.interface_transform:set_position(alledge_lua.vector3.new(-center.x, -center.y, -2))
+	end
+	om.interface_transform:set_rotation(alledge_lua.vector3.new(rotx, roty, 0))
+	alledge_lua.scenenode.attach_node(om.interface_transform, om.model_node);
+--	alledge_lua.scenenode.attach_node(om.model_node, om.bbox_node)
+--[[
+	om.interface_transform = alledge_lua.transformnode.new()
+	om.interface_transform:set_scale(alledge_lua.vector3.new(1/2, 1/2, 1/2))
+	om.interface_transform:set_position(alledge_lua.vector3.new(0, -7.5, -2))
 	om.interface_transform:set_rotation(alledge_lua.vector3.new(0, 0, 0))
 	alledge_lua.scenenode.attach_node(om.interface_transform, om.model_node);
-	alledge_lua.scenenode.attach_node(om.model_node, om.bbox_node)
-
+--	alledge_lua.scenenode.attach_node(om.model_node, om.bbox_node)
+--]]
 	master_objects[master_objects_next_id] = om
 	master_objects_next_id = master_objects_next_id + 1
 	return master_objects_next_id - 1
@@ -149,7 +183,7 @@ load_static_object = function (path)
 	scale = 1/biggest
 	print(biggest)
 	
-	center = (low+high)/2
+	center = (low+high)/2*scale
 	print("Center: " .. center.x .. ", " .. center.y .. ", " .. center.z)
 
 	om.interface_transform = alledge_lua.transformnode.new()
@@ -158,6 +192,8 @@ load_static_object = function (path)
 		om.interface_transform:set_position(alledge_lua.vector3.new(-center.z, -center.y, -2))
 	elseif rotx == 90 then
 		om.interface_transform:set_position(alledge_lua.vector3.new(-center.x, -center.z, -2))
+	else
+		om.interface_transform:set_position(alledge_lua.vector3.new(-center.x, -center.y, -2))
 	end
 	om.interface_transform:set_rotation(alledge_lua.vector3.new(rotx, roty, 0))
 	alledge_lua.scenenode.attach_node(om.interface_transform, om.model_node);
